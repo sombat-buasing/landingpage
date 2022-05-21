d382da14f21bce2b06edd7165ce12a9eebe32032
sombat_buasing <sombat.buasing@gmail.com>
2022-05-21 09:57:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode:'jit',
+  content: [
+    './src/**/*.{js,html}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat','Prompt','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
